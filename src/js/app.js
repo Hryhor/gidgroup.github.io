80602b3d247b6ef44af62bdc3859f1dc54ac9db8
Illia Hryhorenko <hryhorenko.illia@gmail.com>
2022-10-24 23:50:36 +0300

    adaptive version added

--- src/js/app.js
+++ src/js/app.js
@@ -51,6 +51,41 @@ mobileNavButton.onclick = function() {
   document.body.classList.toggle('no-scroll')
 }
 
+/**/ 
+let animItems = document.querySelectorAll('._animation');
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('_active');
+      }else {
+        animItem.classList.remove('_active');
+      }
+    }
+  }
+  function offset(el) {
+    const rect =el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+  animOnScroll();
+}
+
+/**/
 /*const showModalByTime = (selector, time) => {
   setTimeout(() => {
     document.querySelector(selector).style.display = 'block';
